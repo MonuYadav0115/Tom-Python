fbf94faffa1a6552530fba0f45fc15d9fbd57b55
Monu Yadav <monuy7883@gmail.com>
2026-01-03 22:09:02 +0530

    python list

---
+++ 8_list/python9.py
@@ -0,0 +1,49 @@
+
+# removing element 
+
+# 1 pop(index)
+# 2 removed(element)
+# 3 clear()
+# 4 del
+
+#  1 pop(index)
+
+# l = [1,2,3,4,5,6,8,9]
+# l.pop(4)
+# print(l)
+
+# l = [1,2,3,4,5,6]
+# l.pop()
+# print(l)   # last element delete
+
+
+
+# removed(element)
+
+# l = [11,12,13,14,15,16,17,18]
+# l.remove(17)
+# print(l)
+
+
+# l = [11,1,13,15,16,17]
+# l.remove()
+# print(l) # error--> give parameter is maidetary
+
+
+# l = [1,1,13,14,13,14,14,11]
+# l.remove(14)
+# print(l)    # fisrt accurance delete 
+
+# clear ---- delete all element 
+
+# l = [1,1,13,13,13,1,12]
+# l.clear()
+# print(l)
+
+# del  ----> provided indexs which one you want delete just like pop
+
+
+# l = [1,2,13,13,16]
+# del l[2]
+# print(l)
+
