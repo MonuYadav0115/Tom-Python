83aaaf51134b1b7e38be3df6e18b21bff8340fb5
Monu Yadav <monuy7883@gmail.com>
2026-01-05 21:18:17 +0530

    python

---
+++ 8_list/python11.py
@@ -0,0 +1,49 @@
+
+# shuffle a list 
+# 1 
+import random as rd 
+
+# print(rd .random())
+
+
+# 2 print randomm number in integer value 1 to 100
+
+
+# print(rd.randint(1,100))
+
+
+# 3 its print range trhouth number 1 to 10 only odd number 
+
+# print(rd.randrange(1,10,2))
+
+
+# 4 its give 5 random rumber each time different 
+
+# for i in range(5):
+#     print(rd.randint(1,100))
+
+
+#  5 seed method give you same set of number every time 
+
+# rd.seed(10)
+# for i in range(5):
+#     print(rd.randint(1,100))
+
+
+
+
+# 6 choice method give a every time diffent of value in the list 
+
+
+# l = ["a","b","c",10,12,1,5,2.9,True]
+
+# print(rd.choice(l))
+
+
+# if you want more than one number give you [rd.choices(k = 4)]
+
+
+# l = ["a","b",22,3,21,True,221,11,323,]
+
+# print(rd.choices(l,k=4))
+
