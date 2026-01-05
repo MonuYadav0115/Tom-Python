7b960a21edfcbbb81c09718c576136a2c4eac058
Monu Yadav <monuy7883@gmail.com>
2026-01-05 22:06:43 +0530

    python

---
+++ 8_list/python14.py
@@ -0,0 +1,23 @@
+
+# mean and mediun mode 
+
+# mean --> total number of sum and divided by total number of count like over all list element to sum  and divided by number of all element so i will say this is mean
+
+
+#  mediun :--> mediun means  first sorted all list in assending order then hoe is the middle of element this is mediun 
+
+# mode :--> which element repeated num of time this is called mode :
+
+
+import statistics as st 
+
+l = [1,2,3,4,5,6,77,8,9]
+
+mean = st.mean(l)
+median = st.median(l)
+mode = st.mode(l)
+
+
+print(mean)
+print(median)
+print(mode)
