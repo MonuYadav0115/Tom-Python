6768720c771a076bacafa0a11e9ad73f468518b0
Monu Yadav <monuy7883@gmail.com>
2026-01-05 23:49:22 +0530

    python

---
+++ 9_tuple/python1.py
@@ -0,0 +1,44 @@
+
+# Tuple Indroduction 
+
+# What is tuple :-> Tuple is same as List but immutable 
+
+# tuple = (1 , 2 , 3 , "zentrox" , 1.2 , 2+2x , true)
+
+# t1 =()      #tuple 
+# t2 = (3)    # it is not tuple it will think integer value add the , 
+# t3 = (3,)   # tuple 
+# t4 = 5,3,2,3,4,4 # it will think tuple
+ 
+
+
+# 1
+
+t1 = (1,2,3,4,5)
+# print(t1)
+# print(type(t1))
+
+t2 = tuple([1,2,3,4,5,6])
+t3 = tuple ("python")
+t4 = tuple(range(1,5))
+# print(t2)
+# print(t3)
+# print(t4)
+
+t5 = ()
+print(type(t5))
+
+t6 = (3)
+print(type(t6))
+
+t7 = (7,)
+print(type(t7))
+
+t8 = 10,12,15,17,18 
+
+print(type(t8))
+
+
+
+
+
