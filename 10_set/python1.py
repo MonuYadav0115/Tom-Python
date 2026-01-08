b97c0c83b546f4ab86789dbdae8547bc14568cee
Monu Yadav <monuy7883@gmail.com>
2026-01-08 20:39:27 +0530

    pyhon

---
+++ 10_set/python1.py
@@ -0,0 +1,54 @@
+
+# Set introduction 
+
+# collection of element 
+# Distinect Elements mean duplicate not allow
+# Heterogenous 
+# Unordered
+
+# Unoreder collection of distinct elements its heterogenous it is mutable
+
+# creation 
+
+# s1 = {1,2,3,4}
+# s2 = set([1,2,3,4,5,6,7])
+# s3 = set("python")
+# s4 = {empty} # its is not set atlist one element thair
+
+# 1
+
+# s1 = set("python")
+# print(s1)
+
+# s1 = {1}
+# print( type (s1))
+
+
+# s = { 10,20,30,40,50,70}
+
+# for i in s:
+    # print(i)
+
+ 
+
+# s1 = set("python")
+
+# for i in s1:
+#     print(i)
+
+
+
+# s1 = {10,20,30,40,50}
+# s1.add(90)
+# print(s1)
+
+
+# s1 = {10,20,30}
+# s1.add("hello")
+# print(s1)
+
+
+# s1 = {1,12,13,14}
+# s1.pop()
+# print(s1)
+
