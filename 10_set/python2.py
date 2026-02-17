3905df5f9f422c75f5e9f9dfab40a7a652acd572
Monu Yadav <monuy7883@gmail.com>
2026-02-17 11:24:21 +0530

    sss

--- 10_set/python2.py
+++ 10_set/python2.py
@@ -1,1 +1,58 @@
+# Union method
+
+# s1 = {1,2,3,5,7}
+# s2 = {5,7,9,10,11}
+# s3 = s1.union(s2)
+# print(s3)
+
+
+# intersection
+
+# s1 = {1,2,3,5,7}
+# s2 = {5,7,9,10,11}
+# s3 = s1.intersection(s2)
+# print(s3)
+
+
+# intersection_update
+
+# s1 = {1,2,3,5,7}
+# s2 = {5,7,9,10,11}
+# s3 = s1.intersection_update(s2)
+# print(s1)
+
+
+
+# different
+
+# s1 = {1,2,3,5,7}
+# s2 = {5,7,9,10,11}
+# s3 = s1.difference(s2)
+# print(s3)
+
+
+
+# different update 
+
+# s1 = {1,2,3,5,7}
+# s2 = {5,7,9,10,11}
+# s3 = s1.difference_update(s2)
+# print(s1)
+
+
+# symmetric_different
+
+# s1 = {1,2,3,5,7}
+# s2 = {5,7,9,10,11}
+# s3 = s1.symmetric_difference(s2)
+# print(s3)
+
+
+# symmetric_diiferent_update
+
+# s1 = {1,2,3,5,7}
+# s2 = {5,7,9,10,11}
+# s3 = s1.symmetric_difference_update(s2)
+# print(s1)
+
 
