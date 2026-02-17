acc9807339e5e3624c5fbcf06e8138f2de60a9eb
Monu Yadav <monuy7883@gmail.com>
2026-02-17 14:31:45 +0530

    wwww

---
+++ 10_set/python3.py
@@ -0,0 +1,12 @@
+# |--> Union
+# &--> Intersection
+# &=-->Intersection update
+# -  --> difference
+# -= -->differece update
+# ^ -->Symmetric differece 
+# ^= --> Symmetric differece update 
+
+s1 = {1,2,3,4,5,7,9}
+s2 = {9,10,11,7,5}
+s3 = s1 | s2
+print(s3)
---
+++ 10_set/python4.py
@@ -0,0 +1,50 @@
+# adding 
+
+# s1 = {1,2,3,5,7}
+# s1.add(8)
+# print(s1)
+
+# updating 
+
+# s1 = {1,2,3,5,7}
+# s1.update((70,80))
+# print(s1)
+
+
+# s1 = {5,7,9,10,11}
+# s1.update(("python"))
+# print(s1)
+
+
+# s1 = {5,7,9,10,11}
+# s2 = {15,71,91,10,11}
+# s2 = s1.copy()
+# print(id(s2))
+
+
+
+# pop
+
+# s1 = {1,2,3,5,7}
+# s1.pop()
+# print(s1)
+
+# discard 
+
+# s1 = {1,2,3,5,7}
+# s1.discard(3)
+# print(s1)
+
+# s2 = {5,7,9,10,11}
+# s2.discard(70)
+# print(s2)     #same value 
+
+# removed ---> like discard
+
+
+
+# clear
+
+s2 = {5,7,9,10,11}
+s2.clear()
+print(s2)
